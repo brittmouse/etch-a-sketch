114f43e9f1a7eca6afae8937607da9aea277f976
brittmouse <bstancoff@proton.me>
2023-11-02 22:31:21 -0400

    Create generateBox(), clear box div on new runs of function
    This plain implementation works through the console but still lacks the project
    requirements in styling and user input.
    
    TO-DO: User input, styling

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+let userHeight = 16;
+let userWidth = 16;
+const box = document.getElementById("box");
+function generateBox(height, width) {
+  box.innerHTML = "";
+  for (let i = 0; i < height; i++) {
+    const generateRow = document.createElement("div");
+    generateRow.id = "row";
+    generateRow.style.display = "flex";
+    generateRow.style.flexDirection = "row";
+    for (let j = 0; j < width; j++) {
+      const generateDiv = document.createElement("div");
+      generateDiv.id = "one";
+      generateDiv.innerText = "X";
+      generateRow.appendChild(generateDiv);
+    }
+    box.appendChild(generateRow);
+  }
+}
+
 function main() {
   return "Hi mom";
 }
